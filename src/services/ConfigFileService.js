9cf866f03b0c5417d9434b1190a0feeb1bfaf462
gubsitt <itsariya.d@ku.th>
2026-02-17 17:24:09 +0700

    style: Adjust layout and order of search button for mobile view

--- src/services/ConfigFileService.js
+++ src/services/ConfigFileService.js
@@ -1,15 +1,16 @@
 import api from './api';
 
 const ConfigFileService = {
-    getFileContent: async () => {
+     getFileContent: async () => {
         try {
-            const response = await api.get('/api/docker/file/content');
+            const response = await api.get('/api/docker/container/file?path=/app/data/test.conf');
             return response.data;
         } catch (error) {
-            console.error('Error fetching file content:', error);
+            console.error('Error fetching file content from container:', error);
             throw error;
         }
-    }
+    },
+
 };
 
 export default ConfigFileService;
